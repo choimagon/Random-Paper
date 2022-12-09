58d231feec675c95da8381558ed74adb30952bde
choimagon <48877378+choimagon@users.noreply.github.com>
2022-12-09 20:50:53 +0900

    update ver1

---
+++ cal.js
@@ -0,0 +1,66 @@
+
+
+//main
+const Calendar = {
+    init() {
+        const today = new Date();
+        Calendar.showDate(today.getFullYear(), today.getMonth()+1);
+
+        Calendar.year = today.getFullYear();
+        Calendar.month = today.getMonth()+1;
+
+        Calendar.eventhandle();
+    },
+
+    eventhandle(){
+        document.querySelectorAll(".date")
+            .forEach(elem=> {
+                elem.onclick = function(){
+                    const day = this.innerText;
+                    this.id = "click";
+                    console.log(`${Calendar.month} ${day}`);
+                    ymarr.push(`${Calendar.month}월 ${day}일`);
+                    Calendar.callist.innerHTML += `
+                        <div class="ymlist">
+                            <p onclick="Calendar.addMonth(-1)">${Calendar.month}/${day}</p>
+                        </div>
+                    `
+                }
+            });
+    },
+
+    showDate(y, m){
+        const before = document.querySelectorAll(".date");
+        before.forEach(v => v.remove());
+        for(let i = -Calendar.getFirstDay(y, m)+1; i<= Calendar.getLastDate(y, m); i++){
+            Calendar.cal.innerHTML += `
+                <div class="date ${i<1 ? "hidden-date" : ""}">
+                    <p>${i}</p>
+                </div>
+            `
+        }
+        Calendar.eventhandle();
+    },
+
+    getFirstDay(y, m){
+        const date = new Date(`${y}-${m}-1`);
+        return date.getDay();
+    },
+
+    getLastDate(y, m){
+        const date = new Date(y, m+1, 0);
+        return date.getDate();
+    },
+
+    addMonth(m){
+        const date = new Date(
+            Calendar.year, Calendar.month + m -1, 1
+        );
+        Calendar.year = date.getFullYear();
+        Calendar.month = date.getMonth()+1;
+
+        Calendar.showDate(Calendar.year, Calendar.month);
+        Calendar.caltitle.innerHTML =`${Calendar.year}. ${Calendar.month}.`;
+    }
+}
+
--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const pnf = document.querySelector(".people-num-form");
 const pnfinput = document.querySelector(".people-num-form input");
-const df = document.querySelector(".date-form");
-const dfinput = document.querySelector(".date-form input");
 const paper = document.querySelector(".paper-num-form");
 const paperinput = document.querySelector(".paper-num-form input");
 const mainwindowh2 = document.querySelector(".main-window h2");
 const mainwindowh3 = document.querySelector(".main-window h3");
 const rdbtn = document.querySelector(".rd-btn button");
 const person = document.querySelector(".person");
+const message = document.querySelector(".ms");
+
+let ymarr = []; //날짜 저장되는 배열임
 
 pnf.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -15,18 +16,6 @@ pnf.addEventListener("submit", (e)=>{
     mainwindowh2.innerHTML = `제비 갯수 : ${paperinput.value*pnfinput.value}`;
     mainwindowh3.innerHTML = `인원 수 : ${pnfinput.value} | 개인당 뽑을 수 : ${paperinput.value}`;
 });
-
-
-
-const week = ['일', '월', '화', '수', '목', '금', '토'];
-
-df.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    const day = new Date(dfinput.value).getDay();
-    console.log(`${dfinput.value} ${week[day]}`);
-    console.log(new Date(dfinput.value).setDate(1));
-});
-
 
 paper.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -37,29 +26,41 @@ paper.addEventListener("submit", (e)=>{
 
 rdbtn.addEventListener("click", (e)=>{
     e.preventDefault();
-    removeAllchild(person);
-    let randomarr = []
-    // const random = Math.floor(Math.random() * paperinput.value*pnfinput.value)
-    for(let j=0; j<pnfinput.value; j++){
-        for(let i=0; i<paperinput.value; i++){
-            const random = Math.floor(Math.random() * paperinput.value*pnfinput.value)
-            if(randomarr.indexOf(random) === -1){
-                randomarr.push(random)
-                console.log(1);
-            } else{
-                i--
+    if(ymarr.length == paperinput.value*pnfinput.value){
+        removeAllchild(person); 
+        const before = document.querySelectorAll(".ymlist");
+        before.forEach(v => v.remove());
+        let randomarr = []
+        // const random = Math.floor(Math.random() * paperinput.value*pnfinput.value)
+        for(let j=0; j<pnfinput.value; j++){
+            for(let i=0; i<paperinput.value; i++){
+                const random = Math.floor(Math.random() * paperinput.value*pnfinput.value)
+                if(randomarr.indexOf(random) === -1){
+                    randomarr.push(random)
+                } else{
+                    i--
+                }
             }
+            console.log(randomarr);
+        }  
+        for(let a=0; a<pnfinput.value; a++){
+            const li = document.createElement("li");
+            li.id = a;
+            li.innerText = `${a+1} 번째 : `
+            for(let b=0; b<paperinput.value; b++){
+                let an = randomarr[paperinput.value*a+b];
+                li.innerText += `[ ${ymarr[an]} ]`;
+            }
+            person.appendChild(li);
         }
-        console.log(randomarr);
-    }  
-    for(let a=0; a<pnfinput.value; a++){
-        const li = document.createElement("li");
-        li.id = a;
-        li.innerText = `${a+1} 번째 : `
-        for(let b=0; b<paperinput.value; b++){
-            li.innerText += `[ ${randomarr[paperinput.value*a+b]+1} ]`;
+    } else {
+        let sumvalue = paperinput.value*pnfinput.value;
+        message.innerText = `선택 된 제비 : ${ymarr.length}개 ----- `;
+        if(ymarr.length < sumvalue){
+            message.innerText += ` ${sumvalue-ymarr.length}개 추가해주세요.`;
+        } else {
+            message.innerText += ` ${ymarr.length-sumvalue}개 지워주세요.`; 
         }
-        person.appendChild(li);
     }
 })
 
@@ -68,3 +69,4 @@ function removeAllchild(div) {
         div.removeChild(div.firstChild);
     }
 }
+
